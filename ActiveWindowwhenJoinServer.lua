de309a71cd527e908ba754876d582152ef5ad569
Valery <51917480+dmitriyewich@users.noreply.github.com>
2021-03-13 20:23:00 +0300

    v1.3

--- ActiveWindowwhenJoinServer.lua
+++ ActiveWindowwhenJoinServer.lua
@@ -4,14 +4,14 @@ script_description("With this simple script, you won't miss your server connecti
 script_url("https://vk.com/dmitriyewichmods")
 script_dependencies("ffi", "memory", "samp.events")
 script_properties('work-in-pause', 'forced-reloading-only')
-script_version("1.2")
+script_version("1.3")
 script_version_number(03)
 require "lib.moonloader"
 local ffi = require 'ffi'
-local lsampev, sampev = pcall(require, 'samp.events') -- https://github.com/THE-FYP/SAMP.Lua
+local lsampev, sampev = pcall(require, 'samp.events') assert(lsampev, 'Library \'samp.events\' not found.')-- https://github.com/THE-FYP/SAMP.Lua
 local lwm, wm = pcall(require, 'lib.windows.message')
 local lmemory, memory = pcall(require, 'memory')
-local lencoding, encoding = pcall(require, 'encoding') assert(lencoding, 'Library \'encoding\' not found.')			 
+local lencoding, encoding = pcall(require, 'encoding') assert(lencoding, 'Library \'encoding\' not found.')
 
 encoding.default = 'CP1251'
 u8 = encoding.UTF8
@@ -53,7 +53,8 @@ local active = true
 function main()
 	if not isSampfuncsLoaded() or not isSampLoaded() then return end
 	while not isSampAvailable() do wait(100) end
-	hwnd = ffi.C.FindWindowA("Grand theft auto San Andreas", nil)
+	-- hwnd = ffi.C.FindWindowA("Grand theft auto San Andreas", nil)
+	hwnd = ffi.cast('void*', readMemory(0x00C8CF88, 4, false))
     addEventHandler('onWindowMessage', function(msg, wparam, lparam)
         if msg == wm.WM_KEYDOWN or msg == wm.WM_SYSKEYDOWN then
 			if msg == wm.WM_KILLFOCUS then
@@ -75,9 +76,9 @@ function main()
 			end
         end
     end)
-	checklibs() -- эту удалить если не нужна проверка на библиотеки 
+
 	wait(-1)
-	end
+end
 
 function memset(addr)
 	for i = 1, #arr do
@@ -85,69 +86,25 @@ function memset(addr)
 	end
 end
 
-if lsampev then
-	function sampev.onSendClientJoin(version, mod, nickname, challengeResponse, joinAuthKey, clientVer, unknown)
-		if active then
-			lua_thread.create(function()
-				hCurrWnd = ffi.C.GetForegroundWindow()
-				iMyTID   = ffi.C.GetCurrentThreadId()
-				iCurrTID = ffi.C.GetWindowThreadProcessId(hCurrWnd, nil)
-				ffi.C.BlockInput(true)
-				wait(74)
-				ffi.C.AttachThreadInput(iMyTID, iCurrTID, true)
-				ffi.C.SetForegroundWindow(hwnd)
-				ffi.C.OpenIcon(hwnd);
-				ffi.C.SetActiveWindow(hwnd)
-				ffi.C.ShowWindow(hwnd, 3)
-				ffi.C.SwitchToThisWindow(hwnd, true)
-				ffi.C.BringWindowToTop(hwnd)
-				wait(74)
-				ffi.C.BlockInput(false)
-				ffi.C.AttachThreadInput(iMyTID, iCurrTID, false)
-			end)
-		end
-	end
-end
-
-function checklibs() -- с этой строки и до конца
-	if not lsampev then
+function sampev.onSendClientJoin(version, mod, nickname, challengeResponse, joinAuthKey, clientVer, unknown)
+	if active then
 		lua_thread.create(function()
-			print(u8:decode'Подгрузка необходимых библиотек..')
-			--samp.lua 
-			createDirectory(getWorkingDirectory()..'\\lib\\samp')
-			createDirectory(getWorkingDirectory()..'\\lib\\samp\\events')
-			downloadFile('events', getWorkingDirectory()..'\\lib\\samp\\events.lua', 'https://raw.githubusercontent.com/THE-FYP/SAMP.Lua/master/samp/events.lua')
-		 	while not doesFileExist(getWorkingDirectory()..'\\lib\\samp\\events.lua') do wait(0) end
-			downloadFile('raknet', getWorkingDirectory()..'\\lib\\samp\\raknet.lua', 'https://raw.githubusercontent.com/THE-FYP/SAMP.Lua/master/samp/raknet.lua')
-		 	while not doesFileExist(getWorkingDirectory()..'\\lib\\samp\\raknet.lua') do wait(0) end
-			downloadFile('synchronization', getWorkingDirectory()..'\\lib\\samp\\synchronization.lua', 'https://raw.githubusercontent.com/THE-FYP/SAMP.Lua/master/samp/synchronization.lua')
-		 	while not doesFileExist(getWorkingDirectory()..'\\lib\\samp\\synchronization.lua') do wait(0) end
-			downloadFile('bitstream_io', getWorkingDirectory()..'\\lib\\samp\\events\\bitstream_io.lua', 'https://raw.githubusercontent.com/THE-FYP/SAMP.Lua/master/samp/events/bitstream_io.lua')
-		 	while not doesFileExist(getWorkingDirectory()..'\\lib\\samp\\events\\bitstream_io.lua') do wait(0) end
-			downloadFile('core', getWorkingDirectory()..'\\lib\\samp\\events\\core.lua', 'https://raw.githubusercontent.com/THE-FYP/SAMP.Lua/master/samp/events/core.lua')
-		 	while not doesFileExist(getWorkingDirectory()..'\\lib\\samp\\events\\core.lua') do wait(0) end
-			downloadFile('extra_types', getWorkingDirectory()..'\\lib\\samp\\events\\extra_types.lua', 'https://raw.githubusercontent.com/THE-FYP/SAMP.Lua/master/samp/events/extra_types.lua')
-		 	while not doesFileExist(getWorkingDirectory()..'\\lib\\samp\\events\\extra_types.lua') do wait(0) end
-			downloadFile('handlers', getWorkingDirectory()..'\\lib\\samp\\events\\handlers.lua', 'https://raw.githubusercontent.com/THE-FYP/SAMP.Lua/master/samp/events/handlers.lua')
-		 	while not doesFileExist(getWorkingDirectory()..'\\lib\\samp\\events\\handlers.lua') do wait(0) end
-			downloadFile('utils', getWorkingDirectory()..'\\lib\\samp\\events\\utils.lua', 'https://raw.githubusercontent.com/THE-FYP/SAMP.Lua/master/samp/events/utils.lua')
-		 	while not doesFileExist(getWorkingDirectory()..'\\lib\\samp\\events\\utils.lua') do wait(0) end				
-			print(u8:decode'Подгрузка необходимых библиотек окончена. Перезагружаюсь..')
-			noErrorDialog = true
-			wait(1000)
-			thisScript():reload()
-		end)
-		return false
-	end
-	return true
-end
-
-function downloadFile(name, path, link)
-	if not doesFileExist(path) then 
-		downloadUrlToFile(link, path, function(id, status, p1, p2)
-			if status == dlstatus.STATUSEX_ENDDOWNLOAD then
-				print(u8:decode'Файл {006AC2}«'..name..u8:decode'»{FFFFFF} загружен!')
-			end
+			hwnd = ffi.cast('void*', readMemory(0x00C8CF88, 4, false))
+			hCurrWnd = ffi.C.GetForegroundWindow()
+			iMyTID   = ffi.C.GetCurrentThreadId()
+			iCurrTID = ffi.C.GetWindowThreadProcessId(hCurrWnd, nil)
+			ffi.C.BlockInput(true)
+			wait(74)
+			ffi.C.AttachThreadInput(iMyTID, iCurrTID, true)
+			ffi.C.SetForegroundWindow(hwnd)
+			ffi.C.OpenIcon(hwnd);
+			ffi.C.SetActiveWindow(hwnd)
+			ffi.C.ShowWindow(hwnd, 3)
+			ffi.C.SwitchToThisWindow(hwnd, true)
+			ffi.C.BringWindowToTop(hwnd)
+			wait(74)
+			ffi.C.BlockInput(false)
+			ffi.C.AttachThreadInput(iMyTID, iCurrTID, false)
 		end)
 	end
 end
